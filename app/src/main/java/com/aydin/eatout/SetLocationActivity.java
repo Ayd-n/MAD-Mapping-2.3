79ef659bfdbbfaad3f4b63ce45577a3b6037d267
Aydin <3musta19@solent.ac.uk>
2018-02-18 23:24:11 +0000

    Week 4

---
+++ app/src/main/java/com/aydin/eatout/SetLocationActivity.java
@@ -0,0 +1,38 @@
+package com.aydin.eatout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.EditText;
+
+
+import android.content.Intent;
+
+public class SetLocationActivity extends AppCompatActivity implements OnClickListener {
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_location);
+
+        Button setlocation = (Button) findViewById(R.id.btnSetLoc);
+        setlocation.setOnClickListener(this);
+    }
+    public void onClick(View view)
+    {
+        EditText setlattext = (EditText)findViewById(R.id.sl1);
+        EditText setlontext = (EditText)findViewById(R.id.sl2);
+        double latitude = Double.parseDouble(setlattext.getText().toString());
+        double longitude = Double.parseDouble(setlontext.getText().toString());
+
+        Intent intent = new Intent();
+        Bundle bundle = new Bundle();
+
+        bundle.putDouble("com.example.setlat", latitude);
+        bundle.putDouble("com.example.setlon", longitude);
+        intent.putExtras(bundle);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
+}
